8cb965c73d92400481fdf180b812a1900b81591b
raunofreiberg <rauno@weps.ee>
2017-04-05 16:29:48 +0300

    better structure

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// gulpfile.js
+
+// load gulp
+var gulp = require('gulp');
+
+// load gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('scss/*.scss')
+    .pipe(sass())
+    .on('error', function (err) {
+            console.log(err.toString());
+
+            this.emit('end');
+        })
+    .pipe(gulp.dest('css'));
+});
+
+// define the gulp task that will watch for changes in your sass
+gulp.task('watch', function() {
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+// define the default gulp task
+gulp.task('default', ['sass', 'watch']);
